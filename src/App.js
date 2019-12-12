cc75035db38fe2c501c5aa844e933c34c66e9e19
Jon <xiajia@vip.qq.com>
2019-12-12 16:10:22 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const input = useInput("");
+  const list = useList([], input.value);
+  const doAdd = () => {
+    list.doAdd();
+    input.clear();
+  };
+  useEffect(() => {
+    document.title = "Wecome to Tenfold Todo List";
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div className="content">
+        <table>
+          <tbody>{<Items {...list} />}</tbody>
+        </table>
+        <input value={input.value} onChange={input.onChange} />
+        <button onClick={doAdd}> Add </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+function useInput(initValue) {
+  const [value, setValue] = useState(initValue);
+  const onChange = e => setValue(e.target.value);
+  const clear = () => setValue("");
+  return {
+    value,
+    onChange,
+    clear
+  };
+}
+
+function useList(initValue, inputValue) {
+  const [value, setValue] = useState(initValue);
+  const doAdd = () =>
+    setValue(value.concat({ title: inputValue, done: false }));
+  const doDelete = e => setValue(value.filter((v, i) => e !== i));
+  const doDone = e => {
+    const list = [...value];
+    list[e].done = !list[e].done;
+    setValue(list);
+  };
+  return {
+    value,
+    doAdd,
+    doDelete,
+    doDone
+  };
+}
+
+function Items(props) {
+  const { value, doDelete, doDone } = props;
+  return value.map((r, i) => (
+    <tr key={i}>
+      <td>
+        <input
+          type="checkbox"
+          value={i}
+          checked={r.done}
+          onChange={e => doDone(e.target.value)}
+        />
+      </td>
+      <td>{r.done ? <s>{r.title}</s> : <b>{r.title}</b>}</td>
+      <td>
+        <button onClick={() => doDelete(i)}>X</button>
+      </td>
+    </tr>
+  ));
+}
